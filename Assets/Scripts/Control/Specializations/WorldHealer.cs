85c11451d53e0885020514e7f613a62b3987ec47
GraemeMW <graememwilliams@gmail.com>
2021-05-15 17:00:47 -0700

    Water cooler flavour

--- Assets/Scripts/Control/Specializations/WorldHealer.cs
+++ Assets/Scripts/Control/Specializations/WorldHealer.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Frankie.Control
+namespace Frankie.Control.Specialization
 {
     public class WorldHealer : MonoBehaviour
     {
