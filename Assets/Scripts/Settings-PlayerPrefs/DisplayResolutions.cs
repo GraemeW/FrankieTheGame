af16fe928b3d406094dbbb3b5735d726ef0316fd
Graeme Williams <graeme.m.williams@gmail.com>
2025-09-04 17:07:38 -0400

    Fix recommended display resolutions for vertical screen

--- Assets/Scripts/Settings-PlayerPrefs/DisplayResolutions.cs
+++ Assets/Scripts/Settings-PlayerPrefs/DisplayResolutions.cs
@@ -29,31 +29,37 @@ namespace Frankie.Settings
             int longEdge = isShortEdgeHeight ? displayInfo.width : displayInfo.height;
 
             // Iterate & build list
-            int option = 0;
+            int option = divisor;
             bool traversingDown = true;
             List<ResolutionSetting> resolutionSettings = new List<ResolutionSetting>();
             for (int i = 0; i < bestResolutionTryCount; i++)
             {
-                int divisorMod = divisor + option;
-                if (divisorMod <= 0) { option = 1; traversingDown = false; continue; }
+                if (option == 0) { option = divisor + 1; traversingDown = false; continue; }
 
-                int shortEdgeLength = shortEdge / divisorMod;
-                if (longEdge % divisorMod == 0)
+                int shortEdgeLength = shortEdge / option;
+                if (longEdge % option == 0)
                 {
-                    int longEdgeLength = longEdge / divisorMod;
-                    if (shortEdgeLength < shortEdge && longEdgeLength < longEdge)
+                    int longEdgeLength = longEdge / option;
+                    if (!isShortEdgeHeight)
                     {
-                        resolutionSettings.Add(new ResolutionSetting(FullScreenMode.Windowed, longEdgeLength, shortEdgeLength));
+                        float longEdgeTarget = (float)shortEdgeLength * (float)shortEdge / (float)longEdge;
+                        int longEdgeDivisor = Mathf.CeilToInt((float)longEdge / longEdgeTarget);
+                        longEdgeLength = longEdge / longEdgeDivisor;
                     }
-                    else
+
+                    if (shortEdgeLength > shortEdge || longEdgeLength > longEdge // invalid entry
+                        || (shortEdgeLength == shortEdge && longEdgeLength == longEdge)) // use FSW
                     {
-                        option = 0;
-                        traversingDown = false;
+                        if (traversingDown) { option--; } else { option++; }
+                        continue;
                     }
+
+                    int width = isShortEdgeHeight ? longEdgeLength : shortEdgeLength;
+                    int height = isShortEdgeHeight ? shortEdgeLength : longEdgeLength;
+                    resolutionSettings.Add(new ResolutionSetting(FullScreenMode.Windowed, width, height));
                 }
 
-                if (traversingDown) { option--; }
-                else { option++; }
+                if (traversingDown) { option--; } else { option++; }
                 if (resolutionSettings.Count >= count) { break; }
             }
 
