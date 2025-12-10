5ffaa78b3a922f384aa793853b8318d0c4b212a9
Graeme Williams <graeme.m.williams@gmail.com>
2025-12-10 15:52:09 -0500

    Fix includes for save system - compilation issues since UnityEditor.SceneManagement does not get included on build

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 using UnityEditor;
+#if UNITY_EDITOR
 using UnityEditor.SceneManagement;
-using Newtonsoft.Json.Linq;
+#endif
 
 namespace Frankie.Saving
 {
