067ccd81afff7372cded735318ae512b0a88fd9d
Graeme Williams <graeme.m.williams@gmail.com>
2026-03-26 16:06:35 -0400

    Remove errant debug messages that snuck in

--- Assets/Scripts/Core/CameraCinematics/CinematicTrigger.cs
+++ Assets/Scripts/Core/CameraCinematics/CinematicTrigger.cs
@@ -26,10 +26,8 @@ namespace Frankie.Core
             if (!playOnStart) { return; }
 
             bool debugSkipPlay = false;
-            Debug.Log("HERE");
 #if UNITY_EDITOR
             debugSkipPlay = FrankieDebugger.IsCinematicAutoplayDisabled();
-            Debug.Log($"debugSkipPlay: {debugSkipPlay}");
 #endif
             if (!debugSkipPlay)
             {
