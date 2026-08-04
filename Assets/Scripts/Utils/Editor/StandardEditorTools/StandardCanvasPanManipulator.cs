3b6acc44d263d8c93da3e6ae0dfde95380e533ca
Graeme Williams <graeme.m.williams@gmail.com>
2026-08-04 18:48:21 -0400

    Clean up Utils Tools directory

---
+++ Assets/Scripts/Utils/Editor/StandardEditorTools/StandardCanvasPanManipulator.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Frankie.Utils.Editor
+{
+    public class StandardCanvasPanManipulator : MouseManipulator
+    {
+        // State
+        private readonly VisualElement panTarget;
+        private Vector2 panStartMousePosition;
+        private Vector2 panStartOffset;
+        private bool isPanning;
+
+        public StandardCanvasPanManipulator(VisualElement panTarget)
+        {
+            this.panTarget = panTarget;
+            activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse });
+            activators.Add(new ManipulatorActivationFilter { button = MouseButton.MiddleMouse });
+        }
+
+        protected override void RegisterCallbacksOnTarget()
+        {
+            target.RegisterCallback<MouseDownEvent>(OnMouseDown);
+            target.RegisterCallback<MouseMoveEvent>(OnMouseMove);
+            target.RegisterCallback<MouseUpEvent>(OnMouseUp);
+        }
+
+        protected override void UnregisterCallbacksFromTarget()
+        {
+            target.UnregisterCallback<MouseDownEvent>(OnMouseDown);
+            target.UnregisterCallback<MouseMoveEvent>(OnMouseMove);
+            target.UnregisterCallback<MouseUpEvent>(OnMouseUp);
+        }
+
+        private void OnMouseDown(MouseDownEvent mouseDownEvent)
+        {
+            if (isPanning || !CanStartManipulation(mouseDownEvent)) { return; }
+
+            isPanning = true;
+            panStartMousePosition = mouseDownEvent.mousePosition;
+            panStartOffset = new Vector2(panTarget.resolvedStyle.left, panTarget.resolvedStyle.top);
+
+            target.CaptureMouse();
+            mouseDownEvent.StopPropagation();
+        }
+
+        private void OnMouseMove(MouseMoveEvent mouseMoveEvent)
+        {
+            if (!isPanning || !target.HasMouseCapture()) { return; }
+
+            Vector2 delta = mouseMoveEvent.mousePosition - panStartMousePosition;
+            panTarget.style.left = panStartOffset.x + delta.x;
+            panTarget.style.top = panStartOffset.y + delta.y;
+            mouseMoveEvent.StopPropagation();
+        }
+
+        private void OnMouseUp(MouseUpEvent mouseUpEvent)
+        {
+            if (!isPanning || !CanStopManipulation(mouseUpEvent)) { return; }
+
+            isPanning = false;
+            target.ReleaseMouse();
+            mouseUpEvent.StopPropagation();
+        }
+    }
+}
