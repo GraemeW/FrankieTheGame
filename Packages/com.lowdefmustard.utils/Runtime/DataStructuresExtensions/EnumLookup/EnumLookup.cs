586cc593d513847b44071431551db063dc341541
Graeme Williams <graeme.m.williams@gmail.com>
2026-08-23 13:38:31 -0400

    EnumLookup bug fix -- TryGet would return true for any value type due to null checking

--- Packages/com.lowdefmustard.utils/Runtime/DataStructuresExtensions/EnumLookup/EnumLookup.cs
+++ Packages/com.lowdefmustard.utils/Runtime/DataStructuresExtensions/EnumLookup/EnumLookup.cs
@@ -6,14 +6,11 @@ namespace LowDefMustard.Utils
     public class EnumLookup<TEnum, TValue> : EnumLookupBase<TValue> where TEnum : struct, Enum
     {
         private readonly Dictionary<TEnum, TValue> values =  new();
-        private TValue Get(TEnum key) => values.GetValueOrDefault(key);
         private void Set(TEnum key, TValue value) => values[key] = value;
         public override bool TryGet(Enum key, out TValue output)
         {
             output = default;
-            if (key is not TEnum castKey) { return false; }
-            output = Get(castKey);
-            return output != null;
+            return key is TEnum castKey && values.TryGetValue(castKey, out output);
         }
 
         public override bool TrySet(Enum key, TValue input)
